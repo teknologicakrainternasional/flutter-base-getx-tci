8efd1ce5279aa952cd72ad55b7440751d080f218
Agung Budi Prasetyo <agungbudi3@gmail.com>
2023-09-11 11:05:25 +0700

    fix(translation): merge translation

--- lib/_core/translations/_app_translation.dart
+++ lib/_core/translations/_app_translation.dart
@@ -5,9 +5,25 @@ import 'package:get/get_navigation/src/root/internacionalization.dart';
 
 class AppTranslations extends Translations {
   @override
-  Map<String, Map<String, String>> get keys => {
-    ...AppStatusMessage.translation,
-    ...AppErrorMessage.translation,
-    ...AppValidationMessage.translation,
-  };
+  Map<String, Map<String, String>> get keys => _mergeTranslation([
+    AppStatusMessage.translation,
+    AppErrorMessage.translation,
+    AppValidationMessage.translation,
+  ]);
+
+  Map<String, Map<String, String>> _mergeTranslation(
+      List<Map<String, Map<String, String>>> translations,
+      ) {
+    Map<String, Map<String, String>> mergedTranslation = {};
+    for (final translation in translations) {
+      translation.forEach((key, value) {
+        if (mergedTranslation.containsKey(key)) {
+          mergedTranslation[key]!.addAll(value);
+        } else {
+          mergedTranslation[key] = Map.from(value);
+        }
+      });
+    }
+    return mergedTranslation;
+  }
 }
